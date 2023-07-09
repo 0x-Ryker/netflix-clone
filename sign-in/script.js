e464b7ecfb708fcc31a101bccffb9d90123f60f7
Greatman Ugorji <coderyker@gmail.com>
2023-07-09 17:22:55 +0100

    removed some commented tags

--- sign-in/script.js
+++ sign-in/script.js
@@ -63,16 +63,3 @@ password.addEventListener("focusout", (e)=>{
 });
 
 
-/////// Password Toggle Visibility
-
-const togglePassword = document.querySelector('#togglePassword');
-  
-
-  togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye');
-});
-
